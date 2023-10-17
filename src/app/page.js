6c7a3d1c428c9b0bdbc0678a727f06a183451f45
Anna Panteleeva <apanteleevadev@gmail.com>
2023-10-17 18:42:37 +0300

    feat: ssr notes

--- src/app/page.js
+++ src/app/page.js
@@ -1,71 +1,17 @@
-"use client";
 import Link from "next/link";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import styles from "./page.module.css";
+import { getNotes } from '@/app/api/notesApi';
 import NoteList from "@/components/NotesList/NotesList";
-import {
-  selectNotes,
-  selectFilteredNotes,
-  selectDescending,
-  selectSearchQuery,
-  setFilteredNotes,
-  setDescending,
-  setSearchQuery,
-} from "@/store/notesSlice";
-import { fetchNotes } from "@/store/thunk";
+import SortAndFilter from '@/components/SortAndFilter/SortAndFilter';
 
-export default function Home({}) {
-  const initialNotes = useSelector(selectNotes); // изначальные заметки из стора
-  const filteredNotes = useSelector(selectFilteredNotes); // массив отффильтрованных заметок
-  const descending = useSelector(selectDescending); // стейт направления сортировки
-  const query = useSelector(selectSearchQuery); // стейт направления сортировки
+export default async function Home({}) {
 
-  const dispatch = useDispatch();
-  const onSortClick = () => {
-    const sorted = filteredNotes
-      ? [...filteredNotes]
-          .filter((note) => {
-            return (
-              note.title.replace(/\s/g, "").toLowerCase().includes(query) ||
-              note.text.replace(/\s/g, "").toLowerCase().includes(query)
-            );
-          })
-          .sort((a, b) =>
-            descending
-              ? a.text.toLowerCase() < b.text.toLowerCase()
-                ? 1
-                : -1
-              : a.text.toLowerCase() > b.text.toLowerCase()
-              ? 1
-              : -1
-          )
-      : [...initialNotes].sort((a, b) =>
-          descending
-            ? a.text.toLowerCase() < b.text.toLowerCase()
-              ? 1
-              : -1
-            : a.text.toLowerCase() > b.text.toLowerCase()
-            ? 1
-            : -1
-        );
-    dispatch(setDescending(!descending));
-    dispatch(setFilteredNotes(sorted));
-  };
+  const getNotesFromMock = async () => {
+    const notes = await getNotes();
+    return notes;
+  }
 
-  const filterInputs = (data) => {
-    const filtered = [...initialNotes].filter((note) => {
-      return (
-        note.title.replace(/\s/g, "").toLowerCase().includes(data) ||
-        note.text.replace(/\s/g, "").toLowerCase().includes(data)
-      );
-    });
-    dispatch(setFilteredNotes(filtered));
-  };
-
-  useEffect(() => {
-    void dispatch(fetchNotes());
-  }, [dispatch]);
+  const mockNotes = await getNotesFromMock();
 
   return (
     <section className="album py-5 bg-body-tertiary">
@@ -82,35 +28,8 @@ export default function Home({}) {
           </svg>
           <p className={styles.addText}>Создать</p>
         </Link>
-        <div className="mb-3 d-flex gap-3">
-          <button
-            type="button"
-            className="btn btn-dark d-flex align-items-center"
-            onClick={onSortClick}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="currentColor"
-              className="bi bi-arrow-down-up"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"
-              />
-            </svg>
-          </button>
-          <input
-            type="text"
-            className="form-control"
-            value={query}
-            placeholder="Поиск"
-            onChange={(e) => dispatch(setSearchQuery(e.target.value), filterInputs(e.target.value))}
-          />
-        </div>
-        <NoteList filteredNotes={filteredNotes} />
+        <SortAndFilter />
+        <NoteList mockNotes={mockNotes} />
       </div>
     </section>
   );
--- src/components/NotesList/NotesList.js
+++ src/components/NotesList/NotesList.js
@@ -1,16 +1,34 @@
+'use client'
 import NoteCard from "../NoteCard/NoteCard";
+import { useSelector } from "react-redux";
+import { selectFilteredNotes } from "@/store/notesSlice";
 
-export default function NoteList({ filteredNotes }) {
+export default function NoteList({ mockNotes }) {
+  const filteredNotes = useSelector(selectFilteredNotes); // массив отффильтрованных заметок
+
   return (
     <ul className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 p-0">
-      {[...filteredNotes].reverse().map((note) => (
-        <NoteCard
-          key={note.id}
-          text={note.text}
-          title={note.title}
-          id={note.id}
-        />
-      ))}
+      {!filteredNotes
+        ? [...mockNotes]
+            .reverse()
+            .map((note) => (
+              <NoteCard
+                key={note.id}
+                text={note.text}
+                title={note.title}
+                id={note.id}
+              />
+            ))
+        : [...filteredNotes]
+            .reverse()
+            .map((note) => (
+              <NoteCard
+                key={note.id}
+                text={note.text}
+                title={note.title}
+                id={note.id}
+              />
+            ))}
     </ul>
   );
 }
